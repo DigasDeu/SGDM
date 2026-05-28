acaa834947b09ae6a64ae1973ede3b291b87e926
DigasDeu <diegofernandosilva.10@gmail.com>
2026-05-28 14:13:06 -0400

    Atualização

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,18 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "COLE_AQUI_A_API_KEY_REAL",
-  authDomain: "sistema-midia-semsa.firebaseapp.com",
-  projectId: "sistema-midia-semsa",
-  storageBucket: "sistema-midia-semsa.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "SUA_CHAVE_REAL_AQUI",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_NUMERO",
+  appId: "SEU_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
 
 export { auth };
